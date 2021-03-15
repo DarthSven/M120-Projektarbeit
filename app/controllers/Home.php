3231a4c51e51e970871b1120eb905301a248fc38
BrainyXS <brainyxs@gmail.com>
2021-03-15 08:57:09 +0100

    home nümä kapput

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,18 +2,10 @@
 
 class home extends Controller
 {
-    protected $user;
-
-    public function __construct()
-    {
-        $this->user = $this->model('User');
-    }
 
     public function index($name = '')
     {
-        $user = $this->user;
-        $user->name = $name;
-        $this->view('home/index', ['name' => $user->name]);
+        $this->view('home/index', ['name' => $name]);
     }
 }
 

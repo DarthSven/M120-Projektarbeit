5a8f01cd9f69da4c721bce0306a56d8fe48993ae
DarthSven <brunnersven13@gmail.com>
2021-03-29 08:30:40 +0200

    fixed overflow

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -1,6 +1,6 @@
 
 <div class="row">
-    <a class="col-4 divCenter border ticketButton maxHeight"  id="EinFahrt"> <span class="text-white bg-dark p-1 border">Eine Fahrt</span> </a>
-    <a class="col-4 divCenter border ticketButton maxHeight" id="HinRueckFahrt"> <span class="text-white bg-dark p-1 border">Hin- und Rückfahrt<span> </a>
-    <a class="col-4  divCenter border ticketButton maxHeight" id="Mehrfahrtenkarte" > <span class="text-white bg-dark p-1 border">Mehrfahrtenkarte</span></a>
+    <a class="col-sm divCenter border ticketButton maxHeight"  id="EinFahrt"> <span class="text-white bg-dark p-1 border">Eine Fahrt</span> </a>
+    <a class="col-sm divCenter border ticketButton maxHeight" id="HinRueckFahrt"> <span class="text-white bg-dark p-1 border">Hin- und Rückfahrt<span> </a>
+    <a class="col-sm  divCenter border ticketButton maxHeight" id="Mehrfahrtenkarte" > <span class="text-white bg-dark p-1 border">Mehrfahrtenkarte</span></a>
 </div>

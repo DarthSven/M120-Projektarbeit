51208d0edd4db8bda7cbc48901ccb1e4cf5a7c7f
DarthSven <brunnersven13@gmail.com>
2021-03-29 08:18:31 +0200

    trains

--- app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('DB_TYPE', 'mysql');
-    define('DB_HOST', 'musibuda.mysql.db.hostpoint.ch');
-    define('DB_NAME', 'musibuda_SvenBrunner');
-    define('DB_USER', 'musibuda_Sven');
-    define('DB_PASS', 'Projekt2020');
-
-?>
--- app/views/main/index.php
+++ app/views/main/index.php
@@ -1,6 +1,6 @@
 
 <div class="row">
-    <a class="col-4 divCenter border" > Eine Fahrt </a>
-    <a class="col-4 divCenter border"> Hin- und Rückfahrt</a>
-    <a class="col-4  divCenter border"> Mehrfahrtenkarte</a>
+    <a class="col-4 divCenter border ticketButton maxHeight"  id="EinFahrt"> Eine Fahrt </a>
+    <a class="col-4 divCenter border ticketButton maxHeight" id="HinRueckFahrt"> Hin- und Rückfahrt</a>
+    <a class="col-4  divCenter border ticketButton maxHeight" id="Mehrfahrtenkarte" > Mehrfahrtenkarte</a>
 </div>

a41c6cab3291ed01cbfc9d0311ac229fce7d761c
DarthSven <brunnersven13@gmail.com>
2021-03-29 07:58:30 +0200

    reverse stuff

--- app/views/_template/header.php
+++ app/views/_template/header.php
@@ -7,8 +7,6 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="/M120-Projektarbeit/public/css/styles.css" type="text/css" crossorigin="anonymous">
     <title>SBB-Ticketautomat</title>
 </head>

b861ea8a1c2339402f0b3e10086f4d43901da114
BrainyXS <brainyxs@gmail.com>
2021-04-19 08:32:13 +0200

    ka

--- app/views/verbindungen/index.php
+++ app/views/verbindungen/index.php
@@ -1,4 +1,17 @@
-<?php
+<div class="OuterDiv">
+    <div class="center">
+        <h2>Mögliche Verbindungen: </h2>
+        <p>Abfasfahrtssinort: <?=$data["von"]?><br>
+        Ankunftsort: <?=$data["nach"]?><br>
+        Abfahrtszeit: <?=$data["time"]?></p>
 
+        <h1>Lade Verbindungen...</h1>
+        <div id="verbindungenDiv">
 
-?>
+        </div>
+    </div>
+</div>
+<script>
+
+</script>
+

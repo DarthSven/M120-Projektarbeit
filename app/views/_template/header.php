a893486dd20a378b12f88f6716b59be97bd00a80
DarthSven <brunnersven13@gmail.com>
2021-04-19 08:07:27 +0200

    Added "back" button to header.php

--- app/views/_template/header.php
+++ app/views/_template/header.php
@@ -8,12 +8,16 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/656e38591d.js" crossorigin="anonymous"></script>
+    <script src="/M120-Projektarbeit/public/js/script.js"></script>
     <link rel="stylesheet" href="/M120-Projektarbeit/public/css/styles.css" type="text/css" crossorigin="anonymous">
     <title>SBB-Ticketautomat</title>
 </head>
 <body class="grid-Container">
-<div class="row">
+<div class="row ">
+<div class="col  d-flex justify-content-between">
+    <div id='buttonBack' onclick="goBack()" class="d-inline"> <i class="far fa-arrow-alt-circle-left fa-5x"></i> </div>
 
-    <a style="display:flex;justify-content:flex-end;align-items:center;"><i class="fas fa-cog fa-5x"></i></a>
+    <a  href="settings" class="d-inline"><i class="fas fa-cog fa-5x"></i></a>
+</div>
 
 </div>
--- app/views/settings/index.php
+++ app/views/settings/index.php
@@ -1,1 +1,25 @@
-<?php
+
+
+<div class="row mh-100">
+    <form class="form">
+        Font size :
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioBig">
+            <label class="form-check-label" for="fontSizeRadioBig">
+                Big
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioMedium" checked>
+            <label class="form-check-label" for="fontSizeRadioMedium">
+                Medium
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioSmall" checked>
+            <label class="form-check-label" for="fontSizeRadioSmall">
+                Small
+            </label>
+        </div>
+    </form>
+</div>

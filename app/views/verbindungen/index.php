becd4c5936798b6ad40efa8d5c8de41e8688a3e5
BrainyXS <brainyxs@gmail.com>
2021-04-19 08:58:30 +0200

    not working

--- app/views/verbindungen/index.php
+++ app/views/verbindungen/index.php
@@ -1,9 +1,9 @@
 <div class="OuterDiv">
     <div class="center">
         <h2>Mögliche Verbindungen: </h2>
-        <p>Abfasfahrtssinort: <?=$data["von"]?><br>
-        Ankunftsort: <?=$data["nach"]?><br>
-        Abfahrtszeit: <?=$data["time"]?></p>
+        <p>Abfasfahrtssinort: <?= $data["von"] ?><br>
+            Ankunftsort: <?= $data["nach"] ?><br>
+            Abfahrtszeit: <?= $data["time"] ?></p>
 
         <h1>Lade Verbindungen...</h1>
         <div id="verbindungenDiv">
@@ -12,6 +12,20 @@
     </div>
 </div>
 <script>
+    $.ajax({
+        url: "http://transport.opendata.ch/v1/connections?from=<?=$data["von"]?>&to=<?=$data["nach"]?>&time=<?=$data["time"]?>",
+        success: function (result) {
 
+            $("#verbindungenDiv").empty();
+            $("#verbindungenDiv").append("<table>");
+
+            result.connections.forEach(connection => {
+                let abfahrt = new Date(connection.from.departure).toLocaleString();
+                let ankunft = new Date(connection.to.arrival).toLocaleString();
+            });
+            $("#verbindungenDiv").append("</table>");
+
+        }
+    });
 </script>
 

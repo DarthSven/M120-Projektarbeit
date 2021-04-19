3dccc6aa48a49cce260bf769b11ae2606ba2d387
DarthSven <brunnersven13@gmail.com>
2021-04-19 08:31:56 +0200

    header, settings

--- app/views/settings/index.php
+++ app/views/settings/index.php
@@ -1,25 +1,39 @@
 
 
 <div class="row mh-100">
-    <form class="form">
-        Font size :
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioBig">
-            <label class="form-check-label" for="fontSizeRadioBig">
-                Big
-            </label>
+    <form >
+        <div class="form-row">
+            Font size :
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioBig">
+                <label class="form-check-label" for="fontSizeRadioBig">
+                    Big
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioMedium" >
+                <label class="form-check-label" for="fontSizeRadioMedium">
+                    Medium
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioSmall" >
+                <label class="form-check-label" for="fontSizeRadioSmall">
+                    Small
+                </label>
+            </div>
         </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioMedium" checked>
-            <label class="form-check-label" for="fontSizeRadioMedium">
-                Medium
-            </label>
+
+
+                <button  type="button"> <i class="far fa-moon fa-5x"></i></button>
+
+
+
+        <button type="button" onclick="goBack()"  ><i class="far fa-arrow-alt-circle-left fa-5x"></i> </button>
         </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="fontSizeRadio" id="fontSizeRadioSmall" checked>
-            <label class="form-check-label" for="fontSizeRadioSmall">
-                Small
-            </label>
-        </div>
+
+
+
+
     </form>
 </div>

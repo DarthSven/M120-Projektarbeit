f1d2562f9cfabe193e4b1c081dcd498cc528151a
BrainyXS <brainyxs@gmail.com>
2021-06-14 07:39:43 +0200

    test

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -1,7 +1,7 @@
 
 
 <div class="row mh-100">
-    <a class="col-sm divCenter border ticketButton maxHeight h-100" href="fahrplan" id="EinFahrt"> <span class="text-white bg-dark p-1 border">Eine Fahrt</span> </a>
-    <a class="col-sm divCenter border ticketButton maxHeight h-100" href="fahrplan" id="HinRueckFahrt"> <span class="text-white bg-dark p-1 border">Hin- und Rückfahrt<span> </a>
-    <a class="col-sm  divCenter border ticketButton maxHeight h-100" href="fahrplan" id="Mehrfahrtenkarte" > <span class="text-white bg-dark p-1 border">Mehrfahrtenkarte</span></a>
+    <a class="col-sm divCenter border ticketButton maxHeight h-100" href="fahrplan?type=1" id="EinFahrt"> <span class="text-white bg-dark p-1 border">Eine Fahrt</span> </a>
+    <a class="col-sm divCenter border ticketButton maxHeight h-100" href="fahrplan?type=2" id="HinRueckFahrt"> <span class="text-white bg-dark p-1 border">Hin- und Rückfahrt<span> </a>
+    <a class="col-sm  divCenter border ticketButton maxHeight h-100" href="fahrplan?type=3" id="Mehrfahrtenkarte" > <span class="text-white bg-dark p-1 border">Mehrfahrtenkarte</span></a>
 </div>
